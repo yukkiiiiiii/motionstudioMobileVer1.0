235acb181acc99efe4138ffed4a34eb5d8aadea9
lxna-dev <beljohnluna@gmail.com>
2024-03-11 22:13:14 +0800

    hello

---
+++ motionstudioMobile/Inquiries.xaml.cs
@@ -0,0 +1,35 @@
+namespace motionstudioMobile
+{
+    public partial class Inquiries : ContentPage
+    {
+        // Declaration of the InquiryResult property
+        public string InquiryResult { get; set; }
+
+        public Inquiries()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        // The event handler method
+        private async void OnMakeInquiryClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                using HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync("https://api.example.com/data");
+                response.EnsureSuccessStatusCode(); // Throw on error code.
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // Assigning value to InquiryResult property
+                InquiryResult = responseBody;
+            }
+            catch (HttpRequestException ex)
+            {
+                // Handle request errors
+                InquiryResult = $"Request error: {ex.Message}";
+            }
+        }
+    }
+}
